9a91610bb6337c28d3afc8ee3af552117fcc58a0
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2022-12-01 03:33:51 +0400

    2022.12.01PustokDb
    Add

---
+++ PustokDb2022/PustokDb2022/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokDb2022.DAL;
+using PustokDb2022.Models;
+using PustokDb2022.ViewModels;
+using System.Diagnostics;
+
+namespace PustokDb2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PustokDbContext _context;
+
+
+        public HomeController(PustokDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            HomeViewModel viewModel = new HomeViewModel
+            {
+                SpecialBooks = _context.Books.Include(x => x.Authors).Where(x => x.IsSpecial).Take(20).ToList(),
+                NewBooks = _context.Books.Include(x => x.Authors).Where(x => x.IsNew).Take(20).ToList(),
+                DiscountedBooks = _context.Books.Include(x => x.Authors).Where(x => x.DisCountPercent>0).Take(20).ToList(),
+                Sliders = _context.Sliders.ToList(),
+            };
+            return View(viewModel);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PustokDb2022/PustokDb2022/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PustokDb2022.Models;
+using System.Collections.Generic;
+
+namespace PustokDb2022.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Book> SpecialBooks { get; set; }
+        public List<Book> NewBooks { get; set; }
+        public List<Book> DiscountedBooks { get; set; }
+        public List<Slider> Sliders { get; set; }
+        public List<BookImage> BookImages { get; set; }
+    }
+}
